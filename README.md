067dcd8a8572bdd5f8fb20c20e45b393fa5510cb
ramsbaron <sharad@softrams.com>
2020-02-11 13:48:38 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# select-or-enter
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/select-or-enter)
---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  name = 'Viewer';
+  myControl = new FormControl();  
+  options = ['fruit','meat','chocolate','veggies','dairy'];
+}
